2742201e13aafb24887c0e71722b40da7fac0f31
simantgautam <gautam8178838469@gmail.com>
2023-04-13 20:13:36 +0530

    Updates

--- components/UI/Project.jsx
+++ components/UI/Project.jsx
@@ -217,20 +217,6 @@ const Portfolio = () => {
                   />
                   <h6 style={{ background: "none" }}>JS</h6>
                 </div>
-                <div
-                  className={`${classes.service__item}`}
-                  style={{ background: "none" }}
-                >
-                  <SiGooglemaps
-                    style={{
-                      fontSize: "1.6rem",
-                      color: "#fff",
-                      background: "none",
-                      marginRight: "5px",
-                    }}
-                  />
-                  <h6 style={{ background: "none" }}>Google maps API</h6>
-                </div>
               </div>
               <div
                 className={`${classes.git__dep}`}
@@ -261,123 +247,124 @@ const Portfolio = () => {
           </Col>
 
           {/* Hotstar APP */}
-          <div
-            style={{
-              width: "650px",
-              height: "650px",
-              background:
-                "linear-gradient(to top,  rgb(50,55,68,0.8), rgba(1, 237, 255, 0.4))",
-              margin: "50px auto",
-              marginTop: "50px",
-            }}
-            className={`${classes.tab__btns}`}
-          >
-            <Image width="570px" height="220px" alt="img" src={hotstar} />
-            <h2 style={{ background: "none" }}>Hotstar-Clone</h2>
-            <h5 style={{ background: "none" }}>
-              Disney+ Hotstar is an Indian subscription video on-demand
-              over-the-top streaming service owned by Novi Digital Entertainment
-              of Disney Star and operated by Disney Entertainment, both
-              divisions of The Walt Disney Company, featuring domestic Indian
-              film, television and sport content for India itself and its
-              worldwide diaspora.
-            </h5>
-            <h6 style={{ background: "none", marginTop: "10px" }}>
-              {" "}
-              Features :{" "}
-              <span style={{ color: "#fff", background: "none" }}>
-                Login • Signup • Watch Trailer • Subscription • Name update •
-                Search, Filter{" "}
-              </span>
-            </h6>
+          <Col>
             <div
-              className={`${classes.stack__container}`}
-              style={{ background: "none", marginTop: "5px" }}
+              style={{
+                // width: "650px",
+                // height: "650px",
+                background:
+                  "linear-gradient(to top,  rgb(50,55,68,0.8), rgba(1, 237, 255, 0.4))",
+                margin: "50px auto",
+                marginTop: "50px",
+              }}
+              className={`${classes.tab__btns}`}
             >
+              <Image width="570px" height="220px" alt="img" src={hotstar} />
+              <h2 style={{ background: "none" }}>Hotstar-Clone</h2>
+              <h5 style={{ background: "none" }}>
+                Disney+ Hotstar is an Indian subscription video on-demand
+                over-the-top streaming service owned by Novi Digital
+                Entertainment of Disney Star and operated by Disney
+                Entertainment, featuring domestic Indian film, television and
+                sport content for India itself and its worldwide diaspora.
+              </h5>
+              <h6 style={{ background: "none", marginTop: "10px" }}>
+                {" "}
+                Features :{" "}
+                <span style={{ color: "#fff", background: "none" }}>
+                  Login • Signup • Watch Trailer • Subscription • Name update •
+                  Search, Filter{" "}
+                </span>
+              </h6>
               <div
-                className={`${classes.service__item}`}
-                style={{ background: "none" }}
+                className={`${classes.stack__container}`}
+                style={{ background: "none", marginTop: "5px" }}
               >
-                <FaReact
-                  style={{
-                    fontSize: "1.6rem",
-                    color: "#fff",
-                    background: "none",
-                    marginRight: "5px",
-                  }}
-                />
-                <h6 style={{ background: "none" }}>React</h6>
-              </div>
-              <div
-                className={`${classes.service__item}`}
-                style={{ background: "none" }}
-              >
-                <SiRedux
-                  style={{
-                    fontSize: "1.6rem",
-                    color: "#fff",
-                    background: "none",
-                    marginRight: "5px",
-                  }}
-                />
-                <h6 style={{ background: "none" }}>Redux</h6>
-              </div>
+                <div
+                  className={`${classes.service__item}`}
+                  style={{ background: "none" }}
+                >
+                  <FaReact
+                    style={{
+                      fontSize: "1.6rem",
+                      color: "#fff",
+                      background: "none",
+                      marginRight: "5px",
+                    }}
+                  />
+                  <h6 style={{ background: "none" }}>React</h6>
+                </div>
+                <div
+                  className={`${classes.service__item}`}
+                  style={{ background: "none" }}
+                >
+                  <SiRedux
+                    style={{
+                      fontSize: "1.6rem",
+                      color: "#fff",
+                      background: "none",
+                      marginRight: "5px",
+                    }}
+                  />
+                  <h6 style={{ background: "none" }}>Redux</h6>
+                </div>
 
-              <div
-                className={`${classes.service__item}`}
-                style={{ background: "none" }}
-              >
-                <SiChakraui
-                  style={{
-                    fontSize: "1.6rem",
-                    color: "#fff",
-                    background: "none",
-                    marginRight: "5px",
-                  }}
-                />
-                <h6 style={{ background: "none" }}>Chakra UI</h6>
+                <div
+                  className={`${classes.service__item}`}
+                  style={{ background: "none" }}
+                >
+                  <SiChakraui
+                    style={{
+                      fontSize: "1.6rem",
+                      color: "#fff",
+                      background: "none",
+                      marginRight: "5px",
+                    }}
+                  />
+                  <h6 style={{ background: "none" }}>Chakra UI</h6>
+                </div>
+                <div
+                  className={`${classes.service__item}`}
+                  style={{ background: "none" }}
+                >
+                  <AiOutlineMail
+                    style={{
+                      fontSize: "1.6rem",
+                      color: "#fff",
+                      background: "none",
+                      // marginRight: "5px",
+                    }}
+                  />
+                  <h6 style={{ background: "none" }}>EmailJS</h6>
+                </div>
               </div>
               <div
-                className={`${classes.service__item}`}
-                style={{ background: "none" }}
+                className={`${classes.git__dep}`}
+                style={{ background: "none", marginTop: "5px" }}
               >
-                <AiOutlineMail
-                  style={{
-                    fontSize: "1.6rem",
-                    color: "#fff",
-                    background: "none",
-                    marginRight: "5px",
-                  }}
-                />
-                <h6 style={{ background: "none" }}>EmailJS</h6>
+                <motion.a
+                  className={classes.nav__anc}
+                  href="https://github.com/hero007noob/hotstar-clone"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  whileHover={{ scale: 1.2 }}
+                  transition={{ type: "spring", stiffness: 300 }}
+                >
+                  <BsGithub className={classes.nav__icon} />
+                </motion.a>
+                <motion.a
+                  className={classes.nav__anc}
+                  href="https://hotstar-tawny.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  whileHover={{ scale: 1.2 }}
+                  transition={{ type: "spring", stiffness: 300 }}
+                >
+                  <RiShareBoxFill className={classes.nav__icon} />
+                </motion.a>
               </div>
             </div>
-            <div
-              className={`${classes.git__dep}`}
-              style={{ background: "none", marginTop: "5px" }}
-            >
-              <motion.a
-                className={classes.nav__anc}
-                href="https://github.com/hero007noob/hotstar-clone"
-                target="_blank"
-                rel="noopener noreferrer"
-                whileHover={{ scale: 1.2 }}
-                transition={{ type: "spring", stiffness: 300 }}
-              >
-                <BsGithub className={classes.nav__icon} />
-              </motion.a>
-              <motion.a
-                className={classes.nav__anc}
-                href="https://hotstar-tawny.vercel.app/"
-                target="_blank"
-                rel="noopener noreferrer"
-                whileHover={{ scale: 1.2 }}
-                transition={{ type: "spring", stiffness: 300 }}
-              >
-                <RiShareBoxFill className={classes.nav__icon} />
-              </motion.a>
-            </div>
-          </div>
+          </Col>
         </Row>
       </Container>
     </section>
